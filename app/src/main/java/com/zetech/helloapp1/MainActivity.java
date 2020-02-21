14e40bc92a4b1408024454b08ea73b9d7c2d0a0e
Nick <nicolous.kibiwot@gmail.com>
2020-02-21 12:07:11 +0300

    initial version

---
+++ app/src/main/java/com/zetech/helloapp1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.zetech.helloapp1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+/*
+    @NonNull
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        public boolean onCreateOptionsMenu(Menu menu){
+            getMenuInflater().inflate(R.menu.menu - main, menu);
+            return true;
+        }
+        @Override OnCCreateOptionsenu
+        public boolean onOptionsItemSelected(MenuItem item){
+            int id = item.getItemId();
+            if (id == R.id.action_settings) {
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+        */
+          }
+
